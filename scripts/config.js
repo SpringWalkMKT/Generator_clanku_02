1dbc8ca6bf651de64a8fda3abfe7506f0a0c23c0
SpringWalkMKT <zarnikov@springwalk.cz>
2025-10-09 20:17:57 +0200

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,5 @@
 // scripts/config.js
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://<TVŮJ_PROJECT_REF>.supabase.co",
-  SUPABASE_ANON_KEY: "<TVŮJ_ANON_KEY>"
+  SUPABASE_URL: "https://tufuymtiiwlsariamnul.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1ZnV5bXRpaXdsc2FyaWFtbnVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk5MjgwMzcsImV4cCI6MjA3NTUwNDAzN30.sShLazLU7TSJTScrYmZJHQ6kv90pOMVcRb5CiGgM9P0"
 };
